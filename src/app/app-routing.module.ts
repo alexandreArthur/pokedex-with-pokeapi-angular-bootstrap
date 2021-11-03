95ff1ca5ad9683aa3f54ac100d87c6409e3d4182
alexandreArthur <arthur_dda@hotmail.com>
2021-11-03 11:55:37 -0300

    pokedex header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
+import { ListComponent } from './list/list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component:ListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
